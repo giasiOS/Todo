d8961ca80e0edd305f1362e84f69381127c29c9c
Gias <khaitov.gias@gmail.com>
2025-01-07 10:06:24 +0500

    Initial Commit

---
+++ Todo/TodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoApp.swift
+//  Todo
+//
+//  Created by ali on 07/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct TodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
