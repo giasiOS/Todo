a5eb1bb43bd27ff87eaf4f2353d14d2582b8d6df
Gias <khaitov.gias@gmail.com>
2025-01-07 15:33:59 +0500

    [Feature] add BG to list add ItemListRow function with input parametor item , add button on zStack alighnment bottomTrailing, changed listStyle to plain

--- Todo/Views/TodoListView.swift
+++ Todo/Views/TodoListView.swift
@@ -12,19 +12,49 @@ struct TodoListView: View {
     @State private var items: [String] = ["This first Todo.", "Then second one.","Third one."]
     
     var body: some View {
-        ZStack {
-         
-            if !items.isEmpty {
-                List(items, id:\.self) { item in
-                    
+        ZStack(alignment: .bottomTrailing) {
+           
+                Color.toDoBG.ignoresSafeArea()
+                if !items.isEmpty {
+                    List(items, id:\.self) { item in
+                        ItemListRow(item: item)
+                            .listRowBackground(Color(.toDoBG))
+                            .listRowSeparatorTint(.toDoSecondary)
+                    }
+                    .padding(.horizontal)
+                    .listStyle(.plain)
+                } else {
+                    ContentUnavailableView.search
                 }
-            } else {
-                ContentUnavailableView.search
-            }
-        
+                
+                Button {
+                    
+                } label: {
+                    Image(systemName: "plus.circle.fill")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 60, height: 60)
+                }
+                .offset(x: -40,y: -60)
+        }
+            .navigationTitle("To Do List")
+            .toolbarBackground(Color(.toDoBG), for: .navigationBar)
+            .tint(.toDoPrimary)
+    }
+    
+    func ItemListRow(item: String) -> some View {
+        HStack {
+            Image(systemName: "circle")
+                .font(.title2)
+                .foregroundStyle(.toDoRed)
+            
+            Text(item)
+                .font(.system(size: 18))
+                .fontWeight(.medium)
+                .foregroundStyle(.toDoPrimary)
         }
         
-            .navigationTitle("To Do List")
+        .padding()
     }
 }
 
