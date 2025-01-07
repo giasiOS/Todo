bc7bcbc72eb9367b35cceddf5b21ce29908b2571
Gias <khaitov.gias@gmail.com>
2025-01-07 15:52:34 +0500

    [Feature] add AddNewItemView

---
+++ Todo/Views/AddNewItemView.swift
@@ -0,0 +1,18 @@
+//
+//  AddNewItemView.swift
+//  Todo
+//
+//  Created by ali on 07/01/2025.
+//
+
+import SwiftUI
+
+struct AddNewItemView: View {
+    var body: some View {
+        Text("AddNewItemView")
+    }
+}
+
+#Preview {
+    AddNewItemView()
+}
