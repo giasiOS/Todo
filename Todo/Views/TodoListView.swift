f163d3486eb3d6b85e4d2bff2d83579349903825
Gias <khaitov.gias@gmail.com>
2025-01-07 14:29:47 +0500

    [Feature] created Views folder into it added TodoListView, remove ContentView form project

---
+++ Todo/Views/TodoListView.swift
@@ -0,0 +1,18 @@
+//
+//  TodoListView.swift
+//  Todo
+//
+//  Created by ali on 07/01/2025.
+//
+
+import SwiftUI
+
+struct TodoListView: View {
+    var body: some View {
+        Text("ToDo List View")
+    }
+}
+
+#Preview {
+    TodoListView()
+}
