98d05f378edb7a4b28d7fd80d8ccf800fc7d457e
Gias <khaitov.gias@gmail.com>
2025-01-07 15:50:19 +0500

    [Clean] add Docs to thie views

--- Todo/Views/ListItemRow.swift
+++ Todo/Views/ListItemRow.swift
@@ -9,21 +9,22 @@ import SwiftUI
 
 struct ListItemRow: View {
     
+    // ijected item..
     var item: String = ""
     
     var body: some View {
         HStack {
+            // SF symbol ..
             Image(systemName: "circle")
                 .font(.title2)
                 .foregroundStyle(.toDoRed)
                 .fontWeight(.thin)
-            
+            // Todo text ..
             Text(item)
                 .font(.system(size: 18))
                 .fontWeight(.medium)
                 .foregroundStyle(.toDoPrimary)
         }
-        
         .padding()
     }
 }
--- Todo/Views/TodoListView.swift
+++ Todo/Views/TodoListView.swift
@@ -9,14 +9,18 @@ import SwiftUI
 
 struct TodoListView: View {
     
+    // Mock data..
     @State private var items: [String] = ["This first Todo.", "Then second one.","Third one."]
     
     var body: some View {
         ZStack(alignment: .bottomTrailing) {
-           
+            // BG layer..
                 Color.toDoBG.ignoresSafeArea()
+            // Condition if Items isEmpty..
                 if !items.isEmpty {
+                    // List of Items..
                     List(items, id:\.self) { item in
+                        // List Row..
                         ListItemRow(item: item)
                             .listRowBackground(Color(.toDoBG))
                             .listRowSeparatorTint(.toDoSecondary)
@@ -24,9 +28,11 @@ struct TodoListView: View {
                     .padding(.horizontal)
                     .listStyle(.plain)
                 } else {
+                    // Mock Content if list is empty..
                     ContentUnavailableView.search
                 }
                 
+            // Action button to add new todo to the list..
                 Button {
                     
                 } label: {
@@ -37,13 +43,16 @@ struct TodoListView: View {
                 }
                 .offset(x: -40,y: -60)
         }
+        // Title layer..
             .navigationTitle("To Do List")
             .toolbarBackground(Color(.toDoBG), for: .navigationBar)
             .tint(.toDoPrimary)
     }
 }
 
+// Preview ..
 #Preview {
+    // Mock nav stack..
     NavigationStack {
         TodoListView()
     }
